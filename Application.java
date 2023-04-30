6cdeb42af7bf75f061d1bbd926fc3d15ee665713
saikumarsuram <emailsaikumarreddysuram@gmail.com>
2023-04-30 13:28:59 +0530

    New Application.java file added into Devbranch

---
+++ Application.java
@@ -0,0 +1,19 @@
+import java.io.*;
+ 
+// Main class
+class GFG {
+ 
+    // Main driver method
+    public static void main(String args[]) {
+ 
+        // Reading content from file by passing local directory path
+        // where file should exists
+        FileInputStream GFG
+            = new FileInputStream("/Desktop/GFG.txt");
+ 
+        // This file does not exist in the location
+        // This constructor FileInputStream
+        // throws FileNotFoundException which
+        // is a checked exception
+    }
+}
